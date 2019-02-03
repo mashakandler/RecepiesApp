7cf94e1c06f3a06586e1eafa5269abcce49ca0e9
Masha Kandler <mashakandler@gmail.com>
2019-02-03 11:30:50 +0200

    addd list component

--- recepieApp/src/app/app.module.ts
+++ recepieApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
